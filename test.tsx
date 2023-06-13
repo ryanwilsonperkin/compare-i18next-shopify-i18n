2c1abc0955771a0f3b27df002698804982443d46
Ryan Wilson-Perkin <ryanwilsonperkin@gmail.com>
2023-06-13 16:47:56 -0400

    Test scaffolding

--- test.tsx
+++ test.tsx
@@ -1,4 +1,48 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import { I18nContext, I18nManager } from "@shopify/react-i18n";
 
-it('should pass', () => {
-	expect(true).toBe(true);
-});
+function createShopifyI18n(locale: string) {
+  const manager = new I18nManager({ locale });
+  return manager;
+}
+
+function createI18next(lng: string) {
+  const instance = i18next.createInstance();
+  instance.use(initReactI18next);
+  instance.init({
+    resources: {
+      fallback: {
+        translation: {
+          currency: "{{val, currency}}",
+          number: "{{val, number}}",
+          percentage: '{{val, number(style: "percent")}}',
+          datetime: "{{val, datetime}}",
+        },
+      },
+    },
+    fallbackLng: "fallback",
+    lng,
+  });
+  return instance;
+}
+
+test.todo("translate", () => {});
+
+test.todo("formatNumber", () => {});
+test.todo("formatCurrency", () => {});
+test.todo("formatPercentage", () => {});
+test.todo("formatDate", () => {});
+test.todo("formatName", () => {});
+
+test.todo("unformatNumber", () => {});
+test.todo("unformatCurrency", () => {});
+
+test.todo("weekStartDay", () => {});
+test.todo("getCurrencySymbol", () => {});
+test.todo("ordinal", () => {});
+test.todo("numberSymbols", () => {});
+
+test.skip("hasEasternNameOrderFormatter");
+test.skip("identifyScripts");
+test.skip("identifyScript");
